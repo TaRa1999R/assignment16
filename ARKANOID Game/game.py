064fd4f824bf8b38a266599ca42869edbc7298f8
TaRa1999R <rezaietara78@gmail.com>
2023-09-06 18:22:55 +0330

    assignment16

--- ARKANOID Game/game.py
+++ ARKANOID Game/game.py
@@ -4,12 +4,28 @@ import arcade
 class Game ( arcade.Window ) :
 
     def __init__ ( self ) :
-        super().__init__( 600 , 800 , "ARKANOID GAME 🧱⚾")
+        super().__init__( 500 , 700 , "ARKANOID GAME 🧱⚾")
+        self.background = arcade.load_texture ("ARKANOID Game/images/background.png")
+        self.game_over = arcade.load_texture ("ARKANOID Game/images/game over.png")
+        self.win = arcade.load_texture ("ARKANOID Game/images/win.png")
+        self.mode = None
+
 
     def on_draw ( self ) :
         arcade.start_render ()
 
-        arcade.set_background_color ( arcade.color.BLUE )
+        if self.mode == "game_over" :
+            arcade.set_background_color ( arcade.color.BLACK )
+            arcade.draw_lrwh_rectangle_textured ( 0 , 0 , self.width , self.height , self.game_over )
+
+        elif self.mode == "win" :
+            arcade.set_background_color ( arcade.color.BLACK )
+            arcade.draw_lrwh_rectangle_textured ( 0 , 0 , self.width , self.height , self.win )
+
+        else :
+            arcade.set_background_color ( arcade.color.BLUE )
+            arcade.draw_lrwh_rectangle_textured ( 0 , 0 , self.width , self.height , self.background)
+
 
         arcade.finish_render ()
 
